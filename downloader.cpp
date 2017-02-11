78a06440b289fca13404e8d42b6add84f3deb82e
Ish Shah <ishshah2@illinois.edu>
2017-02-11 14:29:20 -0600

    multi file

--- downloader.cpp
+++ downloader.cpp
@@ -1,3 +1,4 @@
+
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
 #include <curlpp/cURLpp.hpp>
@@ -8,20 +9,38 @@
 
 #include <thread>
 
+
 using namespace std::literals;
 
-namespace{
-    void Download(const std::string& url, const std::string& filename){
-        std::ofstream of(filename);
+namespace {
+    unsigned ln = 1;
+    auto Color(const std::string& s) {
+        return "\x1B[32m" + s + ": " + "\x1B[0m";
+    }
+
+    auto Line(int l) {
+        int m = l - ln;
+        ln = l;
+        return "\r"+(m<0?"\33["+std::to_string(-m)+'A':std::string(m,'\n'));
+    }
+
+    std::string getURLFile(const std::string& url) {
+        const auto pos = url.find_last_of("/");
+        return pos == std::string::npos ? url : url.substr(pos+1);
+    }
+
+    void Download(const std::string& url, unsigned line) {
+        std::ofstream of(getURLFile(url));
+
         cURLpp::Easy req;
         req.setOpt(cURLpp::Options::Url(url));
         req.setOpt(cURLpp::Options::NoProgress(false));
         req.setOpt(cURLpp::Options::FollowLocation(true));
-        req.setOpt(cURLpp::Options::ProgressFunction([&](std::size_t total, std::size_t done, auto ...){
-            std::cout << "\r" << done << " of " << total << "bytes downloaded (" << int(total ? done * 100./total :0) << "%)" <<std::flush;
+        req.setOpt(cURLpp::Options::ProgressFunction([&](std::size_t total, std::size_t done, auto...) {
+            std::cout << Line(line) << Color(getURLFile(url)) << done << " of " << total << " bytes downloaded (" << int(total ? done*100./total : 0) << "%)" << std::flush;
             return 0;
         }));
-        req.setOpt(cURLpp::Options::WriteFunction([&](const char* p, std::size_t size, std::size_t nmemb){
+        req.setOpt(cURLpp::Options::WriteFunction([&](const char* p, std::size_t size, std::size_t nmemb) {
             of.write(p, size*nmemb);
             return size*nmemb;
         }));
@@ -29,3 +48,15 @@ namespace{
     }
 }
 
+
+int main() {
+    cURLpp::initialize();
+    unsigned line = 1;
+
+    for(const auto& p: {"http://i.imgur.com/Wt6xNSA.jpg"s,
+                        "http://i.imgur.com/RxfpuNO.jpg"s,
+                        "http://i.imgur.com/TTGRX5D.png"s,
+                        "http://i.imgur.com/LCdmRya.png"s}) {
+        Download(p, line++);
+    }
+}
